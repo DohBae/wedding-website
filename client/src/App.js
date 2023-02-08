6c95e56343c8d6c04ae9c2d808f40dff58082041
DohBae <bdoherty92@msn.com>
2023-02-08 10:55:42 -0500

    Create Layout higher order component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-// import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BroswerRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <div>
         <p>
           Hello there!
         </p>
---
+++ client/src/hocs/Layout.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const Layout = ({ children }) => (
+  <Fragment>
+    <div>
+      {children}
+    </div>
+  </Fragment>
+);
+
+export default Layout;
