a56f20b82366a3994f452945e450c5f14dc8506c
DohBae <bdoherty92@msn.com>
2023-02-08 11:24:29 -0500

    Add nav links to navbar.js

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+const Navbar = ({ toggleSidebar }) => (
+  <nav className='navbar'>
+    <ul className='navbar__list'>
+      <li className='navbar__list__item'>
+        <NavLink className='navbar__link' to='/'>
+          Home
+        </NavLink>
+      </li>
+      <li className='navbar__list__item'>
+        <NavLink className='navbar__link' to='/rsvp'>
+          RSVP
+        </NavLink>
+      </li>
+      <li className='navbar__list__item'>
+        <NavLink className='navbar__link' to='/travel'>
+          Travel
+        </NavLink>
+      </li>
+      <li className='navbar__list__item'>
+        <NavLink className='navbar__link' to='/schedule'>
+          Schedule
+        </NavLink>
+      </li>
+      <li className='navbar__list__item'>
+        <NavLink className='navbar__link' to='/qna'>
+          Q & A
+        </NavLink>
+      </li>
+      <li className='navbar__list__item'>
+        <NavLink className='navbar__link' to='/weddingparty'>
+          Wedding Party
+        </NavLink>
+      </li>
+      <li className='navbar__list__item'>
+        <NavLink className='navbar__link' to='/registry'>
+          Registry
+        </NavLink>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
