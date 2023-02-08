5364d2d9cf5553ff2da2a2ad010fb95af0da6833
DohBae <bdoherty92@msn.com>
2023-02-08 11:12:50 -0500

    Add state to navigation.js to handle sidebar toggling

--- client/src/hocs/Layout.js
+++ client/src/hocs/Layout.js
@@ -1,7 +1,9 @@
 import React, { Fragment } from 'react';
+import Navigation from '../components/Navigation';
 
 const Layout = ({ children }) => (
   <Fragment>
+    <Navigation />
     <div>
       {children}
     </div>
