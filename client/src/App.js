39a92d9eb41837c659896b768d85b4983e0e6d0b
DohBae <bdoherty92@msn.com>
2023-02-08 11:05:05 -0500

    Create routes to web pages in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { BroswerRouter as Router } from 'react-router-dom';
+import Layout from './hocs/Layout';
+import Home from './containers/Home';
+import QnA from './containers/QnA';
+import Registry from './containers/Registry';
+import Rsvp from './containers/Rsvp';
+import Schedule from './containers/Schedule';
+import Travel from './containers/Travel';
+import WeddingParty from './containers/WeddingParty';
 
 function App() {
   return (
-    <div>
-        <p>
-          Hello there!
-        </p>
-
-    </div>
+    <Router>
+      <Layout>
+        <Route path='/' component={Home} />
+        <Route path='/qna' component={QnA} />
+        <Route path='/registry' component={Registry} />
+        <Route path='/rsvp' component={Rsvp} />
+        <Route path='/schedule' component={Schedule} />
+        <Route path='/travel' component={Travel} />
+        <Route path='/weddingparty' component={WeddingParty} />
+      </Layout>
+    </Router>
   );
 }
 
